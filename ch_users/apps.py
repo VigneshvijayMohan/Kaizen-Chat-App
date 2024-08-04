ed159baaa04141b135b18ca766512b16a8aedb77
Vignesh Mohan <so@yahoo.com>
2024-08-05 01:20:35 +0530

    Added Profile and edit

---
+++ ch_users/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChUsersConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ch_users"
--- dj_chat/settings.py
+++ dj_chat/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_cleanup.apps.CleanupConfig",
     "ch_home",
+    "ch_users",
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR/"static"]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media' 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
