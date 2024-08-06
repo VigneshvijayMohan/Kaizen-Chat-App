ef549ec7c7ee468195dc727b47d0ba6316b66be0
Vignesh Mohan <so@yahoo.com>
2024-08-06 17:42:57 +0530

    Private chat enabled

--- ch_kchat/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChKchatConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "ch_kchat"
--- ch_users/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChUsersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "ch_users"
